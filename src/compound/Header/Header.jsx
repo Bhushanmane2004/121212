225f15db2710c5a0a10142956ac87148286f5a4e
Bhushan Mane <bhushanvijaymane@gmail.com>
2024-06-07 19:23:53 +0530

    Update Header.jsx

--- src/compound/Header/Header.jsx
+++ src/compound/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className='header-content'>
         <h2>Your Health, Our AI Care</h2>
         <p>GenHealth Hub is a comprehensive online platform that leverages AI and advanced technology to provide personalized healthcare services. Our mission is to enhance your wellness journey with smart diagnostics, preventive care, and tailored health recommendations. Join us in transforming health management for a better tomorrow.</p>
-        <button>View Menu</button>
+        <button>View Details</button>
       </div>
     </div>
   )
